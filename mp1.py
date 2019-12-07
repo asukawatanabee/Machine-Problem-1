130305ad37ffd1cc10dfbf5532e186de93e17f84
asukawatanabee <54177633+asukawatanabee@users.noreply.github.com>
2019-12-07 16:32:32 +0800

    Add files via upload

---
+++ mp1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 23:16:39 2019
+
+@author: asukawatanabe
+"""
+
+import matplotlib.pyplot as plt
+def f(n):
+    if n < 9:
+        return n*n-7
+    return f(n-10)
+
+if __name__ == '__main__' :
+    x = []
+    y = []
+    for i in range(1,100):
+        x.append(i)
+        y.append(f(i))
+        
+        print(x)
+        print(y)
+        plt.plot(x,y)
+        
+        plt.xlabel('x-axis')
+        plt.ylabel('y-axis')
+        
+        plt.title( 'The graph for f(n)' )
+       
+        plt.show()
